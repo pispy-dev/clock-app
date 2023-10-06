17b7c4bf897b0effae3245116563ccf4f0888518
Spyra Piotr 43655680 <piotr.spyra@ubs.com>
2023-10-06 11:12:32 +0200

    change com.example.* application ID to pl.piospy.dev.*

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.clockapp"
+        applicationId = "pl.piospy.dev.clockapp"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
